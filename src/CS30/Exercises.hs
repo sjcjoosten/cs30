d25692a7e0ebcbbd926983a819b86c1bd1bccff2
Fei <i.james.guo@gmail.com>
2020-10-16 18:33:19 +0800

    Add Prob Word exercise

--- src/CS30/Exercises.hs
+++ src/CS30/Exercises.hs
@@ -2,7 +2,7 @@ module CS30.Exercises (pages) where
 import CS30.Exercises.Data (ExerciseType)
 import CS30.Exercises.SetBasics (rosterEx, powsetEx, setOpsEx)
 import CS30.Exercises.Graphs (graphStub)
-
+import CS30.Exercises.ProbWord ( probBasicEx )
 -- a note on MathQuill (what LaTeX is valid and what LaTeX is not):
 -- http://math.chapman.edu/~jipsen/mathquill/test/MathQuillsymbolsMathJax.html
 -- http://math.chapman.edu/~jipsen/mathquill/test/test.html
@@ -10,6 +10,7 @@ import CS30.Exercises.Graphs (graphStub)
 pages :: [ExerciseType]
 pages = [ rosterEx, powsetEx, setOpsEx -- from SetBasics
         -- , graphStub -- not done yet
+        , probBasicEx
         ]
 
 -- the definition below is to prevent warnings about unused imports
