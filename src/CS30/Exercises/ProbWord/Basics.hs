9c0f5853c21fc22f87bb4dfad910210606348932
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2020-10-16 17:46:56 -0400

    got multiple questions to show up

--- src/CS30/Exercises/ProbWord/Basics.hs
+++ src/CS30/Exercises/ProbWord/Basics.hs
@@ -4,10 +4,7 @@ import           CS30.Exercises.Data
 
 
 basicprob :: [ChoiceTree ([Field], String)]
-basicprob = [ node [ ( [show numDice ++ " fair dice are rolled, find the probability that the sum of the results is less than " ++ show sum
+basicprob = [ nodes [([FText $ show numDice ++ " fair dice are rolled, find the probability that the sum of the results is less than " ++ show sum, FFieldMath "prob"], "1/54")
                         | numDice <- [(2::Int)..4], sum <- [numDice + 1..numDice*6]]
-                     -- , FFieldMath "prob" 
-                     , "1/54"
-                      )
-                    ]          
+                     
            ]
