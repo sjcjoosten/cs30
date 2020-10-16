f33f4f3777832fb2a035b59d8c0787517431d053
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2020-10-16 17:16:14 -0400

    Committing stuff that doesn't work

--- src/CS30/Exercises/ProbWord/Basics.hs
+++ src/CS30/Exercises/ProbWord/Basics.hs
@@ -4,7 +4,9 @@ import           CS30.Exercises.Data
 
 
 basicprob :: [ChoiceTree ([Field], String)]
-basicprob = [ nodes [ ( [FText "3 fair dice are rolled, find the probability that the sum of the results is less than 5", FFieldMath "prob"]
+basicprob = [ node [ ( [show numDice ++ " fair dice are rolled, find the probability that the sum of the results is less than " ++ show sum
+                        | numDice <- [(2::Int)..4], sum <- [numDice + 1..numDice*6]]
+                     -- , FFieldMath "prob" 
                      , "1/54"
                       )
                     ]          
