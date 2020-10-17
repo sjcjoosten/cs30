c111b6575143fc6607c3e4f5097d6c46ac81aa79
Paul <paul.gralla@outlook.com>
2020-10-17 11:37:33 -0400

    Add solutionChecker scaffold - not working yet

--- src/CS30/Exercises/ModN.hs
+++ src/CS30/Exercises/ModN.hs
@@ -3,23 +3,24 @@
 module CS30.Exercises.ModN (modN) where
 import CS30.Data
 import CS30.Exercises.Data
+import qualified Data.Map as Map
 
 
 modN :: ExerciseType
 modN = exerciseType "Modulo N" "Modulo N" "True or False" 
-              [boolTree] genTable unknownFeedback
+              [boolTree] genTable modNFeedback
         where unknownFeedback _ _ pr = pr
 
 genTable :: a -> Exercise -> Exercise
 genTable _ def 
- = def{ eQuestion = [ FText $"Here is an example table"
+ = def{ eQuestion = [ FText $"Yo answer those questions"
                     , FTable [row1,row2,row3,row4,row5] ]
-      , eBroughtBy = ["Sebastiaan Joosten"] }
+      , eBroughtBy = ["Paul Gralla and Joseph Hajjar"] }
  where row1 = [Header (FText "Equation"),Header (FText "True/False") ]
-       row2 = [Cell   (FText "Example 1"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_1") ]
-       row3 = [Cell   (FText "Example 2"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_2") ]
-       row4 = [Cell   (FText "Example 3"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_3") ]
-       row5 = [Cell   (FText "Example 4"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_4") ]
+       row2 = [Cell   (FText "Example 1" ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_1") ]
+       row3 = [Cell   (FText "Example 2" ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_2") ]
+       row4 = [Cell   (FText "Example 3" ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_3") ]
+       row5 = [Cell   (FText "Example 4" ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_4") ]
 
     --    row3 = [Cell   (FFieldBool (FText "0") (FText "2") (Just True) "nr_response"),Cell   (FFieldBool (FText "B") (FText "D") Nothing "letter_response") ]
 
@@ -38,3 +39,12 @@ gen_intPower = ""
 
 gen_fracPower :: String
 gen_fracPower = ""
+
+-- right parameters??
+modNFeedback :: Bool -> Map.Map String String -> ProblemResponse -> ProblemResponse
+modNFeedback a b defaultRsp = correct{prFeedback=[(FText "wrong you mongoose")]}
+
+
+    where
+        wrong = markWrong defaultRsp
+        correct = markCorrect defaultRsp
