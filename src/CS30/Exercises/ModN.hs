1911484b615b95150d1fc4dd404c3e741ee9d6c5
Paul <paul.gralla@outlook.com>
2020-10-17 11:10:47 -0400

    Add Table and functionn scaffold

--- src/CS30/Exercises/ModN.hs
+++ src/CS30/Exercises/ModN.hs
@@ -13,11 +13,28 @@ modN = exerciseType "Modulo N" "Modulo N" "True or False"
 genTable :: a -> Exercise -> Exercise
 genTable _ def 
  = def{ eQuestion = [ FText $"Here is an example table"
-                    , FTable [row1,row2,row3] ]
+                    , FTable [row1,row2,row3,row4,row5] ]
       , eBroughtBy = ["Sebastiaan Joosten"] }
- where row1 = [Header (FText "numbers"),Header (FText "letters") ]
-       row2 = [Cell   (FText "1"      ),Cell   (FText "A") ]
-       row3 = [Cell   (FFieldBool (FText "0") (FText "2") (Just True) "nr_response"),Cell   (FFieldBool (FText "B") (FText "D") Nothing "letter_response") ]
+ where row1 = [Header (FText "Equation"),Header (FText "True/False") ]
+       row2 = [Cell   (FText "Example 1"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_1") ]
+       row3 = [Cell   (FText "Example 2"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_2") ]
+       row4 = [Cell   (FText "Example 3"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_3") ]
+       row5 = [Cell   (FText "Example 4"      ), Cell   (FFieldBool (FText "True") (FText "False") (Just False) "TF_4") ]
+
+    --    row3 = [Cell   (FFieldBool (FText "0") (FText "2") (Just True) "nr_response"),Cell   (FFieldBool (FText "B") (FText "D") Nothing "letter_response") ]
 
 
--- gen_congruent :: Exercise
+-- Inputs for these TBD, maybe random seed?
+
+-- generates a congruent addition example
+gen_addition :: String
+gen_addition = ""
+
+gen_multiplication :: String
+gen_multiplication = ""
+
+gen_intPower :: String
+gen_intPower = ""
+
+gen_fracPower :: String
+gen_fracPower = ""
