fb1986ba8a23785601a1ec5a86409d11adbf1e17
Roberto Gabriel Brito <roberto.a.gabriel.brito.22@dartmouth.edu>
2020-10-21 17:23:14 -0400

    render answer (no parsing)

--- src/CS30/Exercises/ComputeX.hs
+++ src/CS30/Exercises/ComputeX.hs
@@ -13,12 +13,12 @@ genQuestion modEx ex = ex {eQuestion = [ FText "Compute " ] ++ques++
                               [ FText (". Give your answer as a non-negative number less than " ++modulus++ "."), FFieldMath "answer" ] } 
                               where (ques, modulus, _) = unwrap modEx
 
--- Doesn't render answer
+-- Currently, this displays any answer as correct
 genFeedback :: ModEx -> Map.Map String String -> ProblemResponse -> ProblemResponse
-genFeedback _ mStrs defaultRsp = case Map.lookup "answer" mStrs of
-                                  Just v -> trace ("Just v: " ++ show v) $ correct{prFeedback = rsp}
-                                  where correct = markCorrect defaultRsp
-                                        rsp = [FText ("Correct!")]
+genFeedback _ mStrs rsp = trace ("genFeedback: " ++ show mStrs) $ 
+                                 case Map.lookup "answer" mStrs of
+                                    Just v -> markCorrect $ rsp{prFeedback = [FText ("You entered " ++ show v)], prTimeToRead=60}
+                                    Nothing -> markWrong $ rsp{prFeedback = [FText "Error: Server failure."], prTimeToRead=60}
 
 modsEx :: ExerciseType
 modsEx = exerciseType "Numbers" "L?.?" "Modulo p, compute X"
