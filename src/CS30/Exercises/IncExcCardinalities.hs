cd8a4922a1f434ee1a912e07a53b8c4ca353e61a
tvergho <tvergho@gmail.com>
2020-10-22 11:36:37 -0700

    genFeedback function

--- src/CS30/Exercises/IncExcCardinalities.hs
+++ src/CS30/Exercises/IncExcCardinalities.hs
@@ -4,29 +4,37 @@ import CS30.Data
 import CS30.Exercises.Data
 import Data.Aeson as JSON -- don't think I use this
 import Data.Aeson.TH
+import Data.Void(Void)
+import Data.Char
+import qualified Data.Map as Map
+import Debug.Trace
+import Text.Megaparsec
+import Text.Megaparsec.Char
 
-data IncExcProblem = IEP ([[Field]], String)
+data IncExcProblem = IEP ([[Field]], Int)
 $(deriveJSON defaultOptions ''IncExcProblem)
+
+type Parser = Parsec Void String
 
 possibleValues :: [Int]
 possibleValues = [1..200]
 
 choiceTreeList :: [ChoiceTree IncExcProblem]
 choiceTreeList = [ 
-      -- Question type 1: Given |A|, |B|, and |A U B|, find |A ∩ B|.
+      -- Question type 1: Given |A|, |B|, and |A U B|, find |A ∩ B| (or vice versa).
       Branch [ Branch [
                   nodes [IEP ([
                               [FText"|A| ", FMath$ "= "++show(d1)], 
                               [FText"|B| ", FMath$"= "++show(d2)], 
                               [FMath$ "|A \\cup B|", FMath$"= "++show(d3)],
                               [FMath$ "|A \\cap B|"]
-                        ], show ((d1 + d2) - d3))], 
+                        ], (d1+d2-d3))], 
                   nodes [IEP ([
                               [FText"|A| ", FMath$ "= "++show(d1)], 
                               [FText"|B| ", FMath$"= "++show(d2)], 
                               [FMath$ "|A \\cap B|", FMath$"= "++show(d3)],
                               [FMath$ "|A \\cup B|"]
-                        ], show ((d1 + d2) - d3))]
+                        ], (d1+d2-d3))]
                   ]
              | d1 <- possibleValues, d2 <- possibleValues, d3 <- possibleValues, d3 < d1 + d2]
       ]
@@ -37,11 +45,10 @@ incExcCards
               "Inclusion exclusion principle" 
               choiceTreeList
               genExercise 
-              unknownFeedback
-        where unknownFeedback _ _ pr = pr
+              genFeedback
 
 genExercise :: IncExcProblem -> Exercise -> Exercise
-genExercise (IEP (fields, answer)) def 
+genExercise (IEP (fields, _)) def 
  = def{ eQuestion = [ FText $"Given the following cardinalities"
                     , FTable (setCards)
                     , FText $"Give the cardinality of "
@@ -50,3 +57,47 @@ genExercise (IEP (fields, answer)) def
       , eBroughtBy = ["Rachael, Tyler"] }
  where setCards = (map . map) Cell (init fields) -- Displays the fields in a table.
        answerField = (head . last) fields -- Last field of IEP is not displayed, but rather is the expression that the user is told to find the value of.
+
+genFeedback :: IncExcProblem -> Map.Map String String -> ProblemResponse -> ProblemResponse
+genFeedback (IEP (fields, sol)) mStrs pr 
+      = trace ("gen feedback " ++ show mStrs ++ " " ++ show pr ++ show answer) $ 
+            case answer of
+                  Nothing -> trace ("wrong" ++ show rsp) wrong{prFeedback= rsp++(FText "Your answer was "):rspwa}
+                  Just v -> if (v == sol) 
+                              then trace ("correct" ++ show rsp) correct{prFeedback=rsp}
+                              else trace ("wrong" ++ show rsp) wrong{prFeedback=rsp++[FText "Your answer was "]++[FMath (show v)]}
+      where ans = Map.lookup "answer" mStrs
+            answer :: Maybe Int
+            answer = case ans of
+                  Nothing -> Nothing
+                  Just v -> case parse parseAnswer "" v of 
+                        Left _ -> Nothing
+                        Right st -> Just st
+            wrong = markWrong pr
+            correct = markCorrect pr
+            rsp :: [Field]
+            rsp = [FText $ "The cardinality of "]++[answerField]++[FText " is ", FMath (show sol)]
+            rspwa = case ans of
+                Nothing -> [FText "??? (perhaps report this as a bug?)"]
+                Just v -> [FMath v]
+            answerField = (head . last) fields
+
+parseAnswer :: Parser Int
+parseAnswer = unspace digits
+
+unspace :: Parser a -> Parser a
+unspace p = spaces >> p
+
+spaces :: Parser ()
+spaces = many spc >> return ()
+ where spc = string " " <|> string "\t" <|> string "\n" <|> string "\r"
+             <|> string "\\ " <|> string "~" -- when parsing latex, these display as spaces too
+
+digits :: Parser Int
+digits = do ds <- some digit
+            return (foldl1 shiftl ds)
+         where shiftl m n = 10*m+n
+
+digit :: Parser Int
+digit = cvt <$> satisfy isDigit
+  where cvt d = fromEnum d - fromEnum '0'
