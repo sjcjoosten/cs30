b4fd21a262ea2a168ffe675177fa25191fcd2156
Roberto Gabriel Brito <roberto.a.gabriel.brito.22@dartmouth.edu>
2020-10-25 12:42:01 -0400

    use list comprehensions to generate easy questions

--- src/CS30/Exercises/ModularArithmetic/ModExercises.hs
+++ src/CS30/Exercises/ModularArithmetic/ModExercises.hs
@@ -19,113 +19,22 @@ $(deriveJSON defaultOptions ''ModEx)
 unwrap :: ModEx -> (Questions, Modulus, Answer)
 unwrap (ModEx (questions, modulus, answer)) = (questions, modulus, answer)
 
-_trivialExercises :: [ModEx]
-_trivialExercises = 
-    [
-        ModEx (
-            [FMath "17 \\equiv_{1}"], "1", 0
-        ),
-        
-        ModEx (
-            [FMath "23 \\equiv_{1}"], "1", 0
-        ),
-        
-        ModEx (
-            [FMath "37 \\equiv_{1}"], "1", 0
-        ),
-        
-        ModEx (
-            [FMath "81 \\equiv_{1}"], "1", 0
-        ),
-        
-        ModEx (
-            [FMath "16 \\equiv_{10}"], "10", 6
-        ),
-        
-        ModEx (
-            [FMath "24 \\equiv_{10}"], "10", 4
-        ),
-        
-        ModEx (
-            [FMath "33 \\equiv_{10}"], "10", 3
-        ),
-        
-        ModEx (
-            [FMath "49 \\equiv_{10}"], "10", 9
-        ),
-        
-        ModEx (
-            [FMath "48 \\equiv_{3}"], "3", 0
-        ),
-        
-        ModEx (
-            [FMath "44 \\equiv_{11}"], "11", 0
-        ),
-        
-        ModEx (
-            [FMath "21 \\equiv_{7}"], "7", 0
-        ),
-        
-        ModEx (
-            [FMath "63 \\equiv_{7}"], "7", 0
-        ),
-        
-        ModEx (
-            [FMath "24 \\equiv_{3}"], "3", 0
-        )
-    ]
+-- Generates range of values for easy problems
+easyGenInts :: [Int]
+easyGenInts = [10 .. 100]
+
+divisors :: Int -> [Int]
+divisors n = [x | x <- [2..(n - 1)], n `rem` x == 0]
+
+-- Used to create prime moduli
+primes :: [Int]
+primes = [x | x <- [2 .. ], null (divisors x)]
+
+easySets :: [(Questions, Modulus, Answer)]
+easySets = [ ([FMath (show x ++ "\\equiv_{" ++ show y ++ "}")], show y, x `mod` y) | x <- easyGenInts, y <- take 15 primes, y < x]
 
 easyExercises :: [ModEx]
-easyExercises = 
-    [   
-        ModEx (
-            [FMath "-30 \\equiv_{7}"], "7", 5
-        ),
-
-        ModEx (
-            [FMath "-9 \\equiv_{13}"], "13", 4
-        ),
-
-        ModEx (
-            [FMath "83 \\equiv_{7}"], "7", 6
-        ),
-
-        ModEx (
-            [FMath "59 \\equiv_{17}"], "17", 8
-        ),
-
-        ModEx (
-            [FMath "91 \\equiv_{23}"], "23", 22
-        ),
-     
-        ModEx (
-            [FMath "-137 \\equiv_{5}"], "5", 3
-        ),
-
-        ModEx (
-            [FMath "-41 \\equiv_{13}"], "13", 11
-        ),
-
-        ModEx (
-            [FMath "(-38 + 19) \\equiv_{5}"], "5", 1
-        ),
-
-        ModEx (
-            [FMath "(-29 + 7) \\equiv_{3}"], "3", 2
-        ),
-
-        ModEx (
-            [FMath "(-22 \\cdot 6) \\equiv_{17}"], "17", 4
-        ),
-
-        ModEx (
-            [FMath "(-13 \\cdot 4) \\equiv_{19}"], "19", 5
-        ),
-
-        ModEx (
-            [FMath "(25 \\cdot 13) \\equiv_{23}"], "23", 3
-        )
-    ]
+easyExercises = take 500 $ drop 500 $ map (ModEx) easySets
 
 mediumExercises :: [ModEx]
 mediumExercises = 
--- src/CS30/Exercises/ModularArithmetic/exercises.md
+++ src/CS30/Exercises/ModularArithmetic/exercises.md
@@ -4,20 +4,118 @@ the student in. For some, it may be enough to develop a
 pattern (if b divides a, then it is 0, anything mod 10 is
 the last digit etc).
 
-48 mod 3
-13 mod 10
-57 mod 1
-
+_trivialExercises :: [ModEx]
+_trivialExercises = 
+    [
+        ModEx (
+            [FMath "17 \\equiv_{1}"], "1", 0
+        ),
+        
+        ModEx (
+            [FMath "23 \\equiv_{1}"], "1", 0
+        ),
+        
+        ModEx (
+            [FMath "37 \\equiv_{1}"], "1", 0
+        ),
+        
+        ModEx (
+            [FMath "81 \\equiv_{1}"], "1", 0
+        ),
+        
+        ModEx (
+            [FMath "16 \\equiv_{10}"], "10", 6
+        ),
+        
+        ModEx (
+            [FMath "24 \\equiv_{10}"], "10", 4
+        ),
+        
+        ModEx (
+            [FMath "33 \\equiv_{10}"], "10", 3
+        ),
+        
+        ModEx (
+            [FMath "49 \\equiv_{10}"], "10", 9
+        ),
+        
+        ModEx (
+            [FMath "48 \\equiv_{3}"], "3", 0
+        ),
+        
+        ModEx (
+            [FMath "44 \\equiv_{11}"], "11", 0
+        ),
+        
+        ModEx (
+            [FMath "21 \\equiv_{7}"], "7", 0
+        ),
+        
+        ModEx (
+            [FMath "63 \\equiv_{7}"], "7", 0
+        ),
+        
+        ModEx (
+            [FMath "24 \\equiv_{3}"], "3", 0
+        )
+    ]
 
 Easy Problems:
 These problems require little computation. Ideally should
 be used if it is a student's first time wrapping their mind
 around something like the ring of integers mod p.
 
-29 mod 5
-83 mod 7
--30 mod 2
--9 mod 16
+_easyExercises :: [ModEx]
+_easyExercises = 
+    [   
+        ModEx (
+            [FMath "-30 \\equiv_{7}"], "7", 5
+        ),
+
+        ModEx (
+            [FMath "-9 \\equiv_{13}"], "13", 4
+        ),
+
+        ModEx (
+            [FMath "83 \\equiv_{7}"], "7", 6
+        ),
+
+        ModEx (
+            [FMath "59 \\equiv_{17}"], "17", 8
+        ),
+
+        ModEx (
+            [FMath "91 \\equiv_{23}"], "23", 22
+        ),
+     
+        ModEx (
+            [FMath "-137 \\equiv_{5}"], "5", 3
+        ),
+
+        ModEx (
+            [FMath "-41 \\equiv_{13}"], "13", 11
+        ),
+
+        ModEx (
+            [FMath "(-38 + 19) \\equiv_{5}"], "5", 1
+        ),
+
+        ModEx (
+            [FMath "(-29 + 7) \\equiv_{3}"], "3", 2
+        ),
+
+        ModEx (
+            [FMath "(-22 \\cdot 6) \\equiv_{17}"], "17", 4
+        ),
+
+        ModEx (
+            [FMath "(-13 \\cdot 4) \\equiv_{19}"], "19", 5
+        ),
+
+        ModEx (
+            [FMath "(25 \\cdot 13) \\equiv_{23}"], "23", 3
+        )
+    ]
 
 
 Medium Problems:
@@ -33,3 +131,4 @@ They'll need some paper.
 
 16 * 37^-1 mod 11
 -19^5 mod 13
+
