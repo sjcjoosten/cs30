04e0f012704da30accebd8b48dd34ea74fcf7997
tvergho <tvergho@gmail.com>
2020-10-25 10:49:49 -0700

    More question types

--- src/CS30/Exercises/IncExcCardinalities.hs
+++ src/CS30/Exercises/IncExcCardinalities.hs
@@ -23,9 +23,9 @@ possibleValues = [1..200]
 
 choiceTreeList :: [ChoiceTree IncExcProblem]
 choiceTreeList = [
-      -- ChoiceTree of Branches for each variable in the equation |A U B U C| = |A| + |B| + |C| - |A ∩ B| - |A ∩ C| - |B ∩ C| + |A ∩ B ∩ C|, to represent three sets.
-      -- |A U B U C| = a + b + c + d + e + f + g
-      -- It then becomes trivial to create and solve systems of equations.
+      -- ChoiceTree of Branches for each variable in the equation |A U B U C| = |A| + |B| + |C| - |A ∩ B| - |A ∩ C| - |B ∩ C| + |A ∩ B ∩ C|, representing three sets.
+      -- |A U B U C| = a + b + c - d - e - f + g
+      -- It then becomes trivial to create and solve systems of equations for different question types.
       Branch [ Branch [Branch [Branch [Branch [Branch [Branch [
             Branch [
                  -- Question type 1: Given |A|, |B|, and |A U B|, find |A ∩ B| (or vice versa).
@@ -48,8 +48,23 @@ choiceTreeList = [
                   [FMath$ "|A \\cup B \\cup C|", FMath$"= "++show(a+b+c-d-e-f+g)], 
                   [FMath$ "|A \\cup (C \\cap B)|", FMath$"= "++show(a+f-g)],
                   [FMath$ "|A \\cup B|", FText"."]
-            ], (a + b - d))]
-      ] | f <- possibleValues, f < (b-50), f < (c-50), f > g] | e <- possibleValues, e < (a-50), e < (c-50), e > g] | d <- possibleValues, d > g, d < (a-50), d < (b-50)]| c <- [80..180], c > g] | b <- [80..180], b > g] | a <- [80..180], a > g] | g <- [10..70]]
+            ], (a + b - d))],
+            -- Question type 3: Given |A U C|, |B U C|, and |C U (A ∩ B)|, find |A U B U C|.
+            nodes [IEP ([
+                  [FMath$ "|A \\cup C|", FMath$ "= "++show(a+c-e)], 
+                  [FMath$ "|B \\cup C|", FMath$"= "++show(b+c-f)], 
+                  [FMath$ "|C \\cup (A \\cap B)|", FMath$"= "++show(c+d-g)],
+                  [FMath$ "|A \\cup B \\cup C|", FText"."]
+            ], (a + b + c - d - e - f + g))],
+            -- Question type 4: Given |A U C|, |B U C|, and |B U (A ∩ C)|, find |A U (B ∩ C)|.
+            nodes [IEP ([
+                  [FMath$ "|A \\cup C|", FMath$ "= "++show(a+c-e)], 
+                  [FMath$ "|B \\cup C|", FMath$"= "++show(b+c-f)],
+                  [FMath$ "|B \\cup (A \\cap C)|", FMath$"= "++show(b+e-g)],
+                  [FMath$ "|A \\cup (B \\cap C)|", FText"."]
+            ], (a + f - g))]
+      ] | f <- possibleValues, f < (b-50), f < (c-50), f > g] | e <- possibleValues, e < (a-50), e < (c-50), e > g] | d <- possibleValues, d > g, d < (a-50), d < (b-50)]| c <- [120..180], c > g] | b <- [120..180], b > g] | a <- [120..180], a > g] | g <- [10..60]]
+      -- These ranges were selected to ensure that none of the branches are empty given the constraints provided. 
 
 incExcCards :: ExerciseType
 incExcCards
@@ -61,10 +76,11 @@ incExcCards
 
 genExercise :: IncExcProblem -> Exercise -> Exercise
 genExercise (IEP (fields, _)) def 
- = def{ eQuestion = [ FText $"Given the following cardinalities"
+ = def{ eQuestion = [ FText $"Given the following cardinalities:"
                     , FTable (setCards)
                     , FText $"Give the cardinality of "
                     , answerField
+                    , FText $"."
                     , FFieldMath "answer" ]
       , eBroughtBy = ["Rachael, Tyler"] }
  where setCards = (map . map) Cell (init fields) -- Displays the fields in a table.
@@ -73,10 +89,10 @@ genExercise (IEP (fields, _)) def
 genFeedback :: IncExcProblem -> Map.Map String String -> ProblemResponse -> ProblemResponse
 genFeedback (IEP (fields, sol)) mStrs pr 
       = reTime$ case answer of
-                  Nothing -> trace ("wrong" ++ show rsp) wrong{prFeedback= rsp++(FText "Your answer was "):rspwa} -- Error when parsing. 
+                  Nothing -> trace ("wrong" ++ show rsp) wrong{prFeedback= rsp++(FText " Your answer was "):rspwa} -- Error when parsing. 
                   Just v -> if (v == sol) 
                               then trace ("correct" ++ show rsp) correct{prFeedback=rsp} -- Correct answer.
-                              else trace ("wrong" ++ show rsp) wrong{prFeedback=rsp++[FText "Your answer was "]++[FMath (show v)]} -- Incorrect answer.
+                              else trace ("wrong" ++ show rsp) wrong{prFeedback=rsp++[FText " Your answer was "]++[FMath (show v), FText "."]} -- Incorrect answer.
       where ans = Map.lookup "answer" mStrs -- Raw user input. 
             answer :: Maybe Int
             answer = case ans of
@@ -90,7 +106,7 @@ genFeedback (IEP (fields, sol)) mStrs pr
             rsp = [FText $ "The cardinality of "]++[answerField]++[FText " is ", FMath (show sol), FText "."] -- Displays the correct answer.
             rspwa = case ans of -- Displays the raw user input. 
                 Nothing -> [FText "??? (perhaps report this as a bug?)"]
-                Just v -> [FMath v]
+                Just v -> [FMath v, FText "."]
             answerField = (head . last) fields
 
 parseAnswer :: Parser Int
