49e242ee0e098655a8c9011bdda007c5abede39f
Lucas Boebel <blucas@Lucass-MacBook-Pro.local>
2020-10-25 23:46:11 -0400

    comments

--- src/CS30/Exercises/CombinatoricsIntegers.hs
+++ src/CS30/Exercises/CombinatoricsIntegers.hs
@@ -1,23 +1,15 @@
-{-Combinatorics: Integer Problems
-Conceived by Sean
-Your program should generate basic counting problems about integers, involving digit
-placement, divisibility, summing the digits, and perhaps more of your choosing.
-The general syntax looks like &quot;How many ____ integers are there such that ____ ?&quot; Here are
-some examples:
-Here are some examples:
+{- CombinatoricsIntegers.hs
+
+generates basic counting problems about integers, involving digit
+placement, divisibility, and summing the digits
+
+Here are some examples of possible questions:
 ● How many 5 digit positive integers are there such that it is divisible by 3, and that 9 is a
 digit? (divisibility, digit placement)
 ● How many 6 digit positive integers are there such that the sum of the digits is at most
 51? (summing the digits)
 ● How many 4 digit positive integers are there such that all digits are different? (custom
-condition)
-The numbers and problem-specific words used should be chosen dynamically, but be
-careful that the problems are coherent when the pieces are put together.
-The user should input an integer solution for any of these problems. Keep in mind that
-combinatorics solutions explode as the parameters grow, so don&#39;t let them get too big.
-The user&#39;s answer should simply be checked for equality against the integer solution.
-As extra challenge, consider generating problems about positive rational numbers (i.e
-any number of the form a/b, where a and b are positive integers).-}
+condition) -}
 
 {-# LANGUAGE TemplateHaskell #-}
 module CS30.Exercises.CombinatoricsIntegers where
