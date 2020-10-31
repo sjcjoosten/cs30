1f69bd30eb7dbe6a5def32c958b38cb086b50fd4
Paul <paul.gralla@outlook.com>
2020-10-31 16:11:46 -0400

    Start on rule parser

--- src/CS30/Exercises/SetCardinalitiesProofs/RuleParser.hs
+++ src/CS30/Exercises/SetCardinalitiesProofs/RuleParser.hs
@@ -1,9 +1,35 @@
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
 
+type Parser = Parsec Void String
 data Symb = Add | Minus | Intersection | Union | Powerset | Card | Const Char
 data Expr = Var Symb | Op Symb [Expr]
 
 data Law = Law String Equation
 type Equation = (Expr, Expr)  -- (left,right)
+
+
+
+pLaw :: Parser Expr -> Parser Law
+pLaw pExpr = do 
+    lawName  <- parseUntil ':'
+    _ <- ":"
+    lhs <- pExpr
+    _ <- "="
+    rhs <- pExpr
+    return (Law lawName (lhs, rhs))
+
+parseUntil c = (do _ <- satisfy (== c)
+                return []) <|>
+             (do c <- satisfy (const True)
+                rmd <- parseUntil c
+                return (c:rmd)
+             )
+
+
+
+
 
 -- Given Rules:
 -- |A \cup B| = |A| + |B| - |A \cap B|
