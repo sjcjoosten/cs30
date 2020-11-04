e110839ce78773936b5fa13020b274d507ffe41e
Bennett <bennettdclark@gmail.com>
2020-11-04 13:52:38 -0500

    added initial proof generation
    may be buggy, only tested with "\\neg(p \\vee q)" as initial expression
    still need:
    - generating initial expressions
    - converting from proofs/expressions into something we can display
    - displaying actual assignment
    - add distributivity laws...

--- src/CS30/Exercises/LogicRewriting.hs
+++ src/CS30/Exercises/LogicRewriting.hs
@@ -18,7 +18,7 @@ data Expr = Var Char          -- variable like 'p' or 'q'
           | Or Expr Expr      -- <expr> \vee <expr>
           | Implies Expr Expr -- <expr> \Rightarrow <expr>
           | Neg Expr          -- negation (\neg <expr>)
-    deriving Show
+    deriving (Eq, Show)
 
 type Parser = ParsecT Void String Identity
 
@@ -112,3 +112,88 @@ parseTerm = parens parseExpr <|> parseConst <|> parseVar
 -- parse a full expression (using makeExprParser)
 parseExpr :: Parser Expr
 parseExpr =  makeExprParser parseTerm operatorTable
+
+-- GENERATING PROOF --
+
+data Proof = Proof Expr [Step] deriving Show
+type Step = (String, Expr) -- law name and resulting expression 
+
+-- 
+getDerivation :: [Law] -> Expr -> Proof
+getDerivation laws e = Proof e (manyStep laws e)
+
+-- 
+manyStep :: [Law] -> Expr -> [Step]
+manyStep laws e = if null steps then []
+                  -- ^ change the condition here to reflect negation??
+                  else step : manyStep laws (snd step)
+                  where rws expr = [(name,e') | Law name eqn <- laws,
+                                                e' <- rewrites eqn e,
+                                                e' /= e]
+                        steps = rws e
+                        step = head steps
+
+-- 
+rewrites :: Equation -> Expr -> [Expr]
+rewrites (lhs, rhs) e = 
+    case matchExpr lhs e of
+        []         -> recurse e
+        (sub:subs) -> [apply s rhs | s <- (sub:subs)]
+    where recurse (Var _)         = []
+          recurse (Const _)       = []
+          recurse (Neg e)         = [Neg e' | e' <- rewrites (lhs,rhs) e]
+          -- TODO: refactor data structure with some sort of BinOp 
+          recurse (And e1 e2)     = [And e1' e2 | e1' <- rewrites (lhs, rhs) e1] ++
+                                    [And e1 e2' | e2' <- rewrites (lhs, rhs) e2]
+          recurse (Or e1 e2)      = [Or e1' e2 | e1' <- rewrites (lhs, rhs) e1] ++
+                                    [Or e1 e2' | e2' <- rewrites (lhs, rhs) e2]
+          recurse (Implies e1 e2) = [Implies e1' e2 | e1' <- rewrites (lhs, rhs) e1] ++
+                                    [Implies e1 e2' | e2' <- rewrites (lhs, rhs) e2]
+
+type Subst = [(Char, Expr)]
+
+-- 
+matchExpr :: Expr -> Expr -> [Subst]
+matchExpr (Var v) e = [ [(v,e)] ]
+matchExpr (Const i) (Const j) | i == j = [[]]
+matchExpr (Const _) _ = []
+matchExpr (Neg e1) (Neg e2) = matchExpr e1 e2
+matchExpr (Neg _) _  = []
+matchExpr (And e1 e2) (And e3 e4)
+    = combine (matchExpr e1 e3) (matchExpr e2 e4)
+matchExpr (And _ _) _  = []
+matchExpr (Or e1 e2) (Or e3 e4) 
+    = combine (matchExpr e1 e3) (matchExpr e2 e4)
+matchExpr (Or _ _) _  = []
+matchExpr (Implies e1 e2) (Implies e3 e4) 
+    = combine (matchExpr e1 e3) (matchExpr e2 e4)
+matchExpr (Implies _ _) _  = []
+
+-- e.g. trying to matchExpr "p ∧ q" to "p ∧ (p ∨ q)"
+--  we first matchExpr the left and right hand sides of the binary op "∧"
+--  resulting in [[('p', Var p)]] and [[('q', And (Var p) (Var q))]]
+--  we want to have [[('p', Var p), ('q', And (Var p) (Var q))]]
+-- 
+--  alternately... combine ["a"] ["bc"] = ["ab", "ac"]
+combine :: [Subst] -> [Subst] -> [Subst]
+combine xs ys = [x ++ y | x <- xs, y <- ys, compatible x y] 
+                where compatible x y = and [e1 == e2 
+                                           | (v1,e1) <- x, 
+                                             (v2,e2) <- y,
+                                             v1 == v2]
+
+-- 
+apply :: Subst -> Expr -> Expr
+apply subst (Var v)         = lookupInSubst v subst
+apply subst (Const b)       = Const b
+apply subst (Neg e)         = Neg (apply subst e)
+apply subst (And e1 e2)     = And (apply subst e1) (apply subst e2)
+apply subst (Or e1 e2)      = Or (apply subst e1) (apply subst e2)
+apply subst (Implies e1 e2) = Implies (apply subst e1) (apply subst e2)
+
+-- 
+lookupInSubst :: Char -> Subst -> Expr
+lookupInSubst v ((v', expr):rm)
+    | v == v'   = expr
+    | otherwise = lookupInSubst v rm
+lookupInSubst _ [] = error "Substitution was not complete"
