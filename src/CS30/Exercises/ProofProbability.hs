3b865b717c6f72a5c18ca663eb9fc1f857e8ac81
mengdge <mengdge@gmail.com>
2020-11-08 23:12:20 -0500

    add some details

--- src/CS30/Exercises/ProofProbability.hs
+++ src/CS30/Exercises/ProofProbability.hs
@@ -3,27 +3,27 @@ module CS30.Exercises.ProofProbability where
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import           CS30.Exercises.Probability.SolutionChecker
-import Control.Monad.Combinators.Expr
-import qualified Text.Megaparsec.Char.Lexer as L
 import CS30.Exercises.Data
-import Data.Text hiding (map)
 
 
 data FracExpr 
     = FConst Rational -- the constants 0 and 1
-    | FVar String -- basic evert : A, B 
+    | FVar Char -- basic evert : A, B 
     | FExpr FracExpr FracExpr -- ^ probability expression like Pr[A|B].
     | FBin MathExpr FracExpr FracExpr -- ^ multiplication
     | AndEvent FracExpr FracExpr  -- ‘and’ \wedge and ‘or’ \vee  
     | OrEvent FracExpr FracExpr 
-    | NegaEvent FracExpr FracExpr  -- event negation \neg
+    | NegaEvent FracExpr  -- event negation \neg
     deriving (Show, Eq)
 
-data Law = Law {lawName :: String, lawEq :: Equation}
+data Law = Law {lawName :: String, lawEq :: Equation} deriving Show
 type Equation = (FracExpr,FracExpr)
 
+law1 :: Law
+law1 = Law "DeMorgan" (NegaEvent (AndEvent (FVar 'A') (FVar 'B')), OrEvent (NegaEvent (FVar 'A')) (FVar 'B'))
+
 -- laws given in the Final Assignmentss
-deMorgan1 :: String
+{-deMorgan1 :: String
 deMorgan1 =  "DeMorgan: \\neg (A \\wedge B) = \\neg A \\vee \\neg B"
 deMorgan2 :: String
 deMorgan2 = "DeMorgan: \\neg (A \\vee B) = \\neg A \\wedge \\neg B"
@@ -40,32 +40,44 @@ emptyElimiV = "Empty-set elimination: \\emptyset \\wedge A = \\emptyset"
 negaProba :: String
 negaProba = "Negation in probability: Pr[\\neg A] = 1 - Pr[A]"
 difference :: String
-difference = "Difference: Pr[A \\wedge \\neg B] = Pr[A] - Pr[A \\wedge B]"
+difference = "Difference: Pr[A \\wedge \\neg B] = Pr[A] - Pr[A \\wedge B]"-}
 
 -- x - y = x + (nagate y)
 -- lhs : x - y; rhs : x + (nagate y)
 parseLaws :: Parser Law
-parseLaws = do {lawName <- parseUntil ':';
+parseLaws = do {lawname <- parseUntil ':';
                 lt_spaces;
                 lhs <- fractional_parser;
                 _ <- string "=";
                 rhs <- fractional_parser;
-                return (Law lawName (lhs,rhs))}
+                return (Law lawname (lhs,rhs))}
 
-parseUntil :: String -> Parser String
-parseUntil = fmap pack . manyTill anyChar . string
+parseUntil :: Char -> Parser String 
+parseUntil c
+  = (do _ <- satisfy (== c)
+        return []) 
+    <|> 
+    (do accum <- satisfy (const True) 
+        rmd <- parseUntil c 
+        return (accum:rmd) 
+        )
 
-laws :: [String] 
-laws = [deMorgan1, deMorgan2, dbNegat, omegaElimiW, emptyElimiW,negaProba,difference]
+{-laws :: [String] 
+laws = [deMorgan1, deMorgan2] -- dbNegat, omegaElimiW, emptyElimiW]-}
+
+laws1 :: [Law]
+laws1 = [law1]
 
 parsedLaws :: [Law]
-parsedLaws = map strToLaw laws
+parsedLaws = laws1
+    --Prelude.map strToLaw laws
 
 strToLaw :: String -> Law 
 strToLaw law =  case parse parseLaws "" law of
                 Right st -> st
 
-data Proof = Proof FracExpr [FracStep]
+
+data Proof = Proof FracExpr [FracStep] deriving (Show, Eq)
 type FracStep = (String, FracExpr)
 
 -- generate two event expression from a set of base events
@@ -75,10 +87,10 @@ genEveExper = undefined
 -- find proof in terms of laws
 -- check lhs and then change to rhs if the lhs are same
 getDerivation :: [Law] -> FracExpr -> Proof
-getDerivation laws e 
+getDerivation lawss e 
     = Proof e (multiSteps e)
     where multiSteps e' = case [(lawName law , res)
-                    | law <- laws
+                    | law <- lawss
                     , res <- getStep (lawEq law) e'] of  -- match rhs 
                 [] -> [] 
                 ((nm, e''):_) -> (nm, e'') : multiSteps e''
@@ -90,8 +102,8 @@ getDerivation laws e
 getStep :: Equation -> FracExpr -> [FracExpr]
 getStep (lhs, rhs) expr 
     = case matchE lhs expr of
-        [] -> recurse expr 
-        (subst:_) -> [apply subset rhs]
+        Nothing -> recurse expr 
+        Just subst -> [apply subst rhs]
     where 
         recurse (FBin op e1 e2)   = [FBin op e1' e2 | e1' <- getStep(lhs, rhs) e1] ++  
                                      [FBin op e1 e2' | e2' <- getStep(lhs, rhs) e2]
@@ -101,13 +113,13 @@ getStep (lhs, rhs) expr
                                      [AndEvent e1 e2'| e2' <- getStep (lhs, rhs) e2]
         recurse (OrEvent e1 e2)   = [OrEvent e1' e2| e1' <- getStep (lhs, rhs) e1] ++
                                      [OrEvent e1 e2'| e2' <- getStep (lhs, rhs) e2]
-        recurse (NegaEvent e1 e2) = [NegaEvent e1' e2| e1' <- getStep (lhs, rhs) e1] ++
-                                     [NegaEvent e1 e2'| e2' <- getStep (lhs, rhs) e2]                        
+        recurse (NegaEvent e1) = [NegaEvent e1'| e1' <- getStep (lhs, rhs) e1] 
+
         recurse _ = []
 
 -- check the lhs of laws
 -- create a substitution list 
-type Substitution = [(String, FracExpr)]
+type Substitution = [(Char, FracExpr)]
 matchE :: FracExpr -> FracExpr -> Maybe Substitution
 matchE (FVar nm) expr                     = Just [(nm, expr)]
 matchE (FConst i) (FConst j)              | i == j = Just []
@@ -117,7 +129,8 @@ matchE (FBin op e1 e2) (FBin op1 e3 e4)   |op == op1 = matchHelper (matchE e1 e3
 matchE (FBin _ _ _) _                     = Nothing
 matchE (AndEvent e1 e2) (AndEvent e3 e4)  = matchHelper (matchE e1 e3)(matchE e2 e4) 
 matchE (OrEvent e1 e2) (OrEvent e3 e4)    = matchHelper (matchE e1 e3)(matchE e2 e4) 
-matchE (NegaEvent e1 e2) (NegaEvent e3 e4)  = matchHelper (matchE e1 e3)(matchE e2 e4) 
+matchE (NegaEvent e1) (NegaEvent e3)      = matchE e1 e3
+matchE _ _                                = Nothing
 
 
 matchHelper :: Maybe Substitution -> Maybe Substitution -> Maybe Substitution
@@ -134,14 +147,14 @@ combineTwoSubsts  s1 s2
 -- change the lhs
 apply :: Substitution -> FracExpr -> FracExpr
 apply subst (FVar nm)           = lookupInSubst nm subst 
-apply subst (FConst i)          = FConst i
+apply _ (FConst i)          = FConst i
 apply subst (FExpr e1 e2)       = FExpr (apply subst e1) (apply subst e2) 
 apply subst (FBin op e1 e2)     = FBin op (apply subst e1) (apply subst e2) 
 apply subst (AndEvent e1 e2)    = AndEvent (apply subst e1) (apply subst e2) 
 apply subst (OrEvent e1 e2)     = OrEvent (apply subst e1) (apply subst e2)  
-apply subst (NegaEvent e1 e2)   = NegaEvent (apply subst e1) (apply subst e2)   
+apply subst (NegaEvent e1)      = NegaEvent (apply subst e1) 
 
-lookupInSubst :: String -> [(String, p)] -> p
+lookupInSubst :: Char -> [(Char, p)] -> p
 lookupInSubst nm ((nm', v) : rm)
     | nm == nm' = v 
     | otherwise = lookupInSubst nm rm 
@@ -162,40 +175,46 @@ fractional_parser = do {lt_spaces;
                         return (FConst r)}
                     <|>
                     do {string "Pr[";
-                        e1 <- basEvent_parser;
+                        e1 <- fractional_parser;
                         string "|";
-                        e2 <- basEvent_parser;
+                        e2 <- fractional_parser;
                         string "]";
                         lt_spaces;
                         return (FExpr e1 e2)
                     }
                     <|>
-                    do {string "Pr[";
-                        e1 <- basEvent_parser;
+                    do {
+                        e1 <- fractional_parser;
                         string "\\wedge";
-                        e2 <- basEvent_parser;
-                        string "]";
+                        e2 <- fractional_parser;
+                    
                         lt_spaces;
                         return (AndEvent e1 e2)
                     } 
                     <|>
-                    do {string "Pr[";
-                        e1 <- basEvent_parser;
+                    do {
+                        e1 <- fractional_parser;
                         string "\\vee";
-                        e2 <- basEvent_parser;
-                        string "]";
+                        e2 <- fractional_parser;
+                        
                         lt_spaces;
                         return (OrEvent e1 e2)
                     }
-                    do {string "Pr[";
-                        e1 <- basEvent_parser;
+                    <|>
+                    do {
+                        e1 <- fractional_parser;
                         string "\\neg";
-                        e2 <- basEvent_parser;
-                        string "]";
                         lt_spaces;
-                        return (NegaEvent e1 e2)
+                        return (NegaEvent e1)
                     }
+                    <|>
+                    do {
+                        e1 <- basEvent_parser;
+                        return (e1)
                     }
+                    } 
+
+
 
 -- codes copied from Cardinality.hs
 data MathExpr 
@@ -205,7 +224,7 @@ data MathExpr
     deriving (Show, Eq)
 
 -- ^here lets us parse things like (3)(3) = 3*3
-operatorTable :: [[Operator Parser MathExpr]]
+{- operatorTable :: [[Operator Parser MathExpr]]
 operatorTable = [[binary "\\cdot" Mult, binary "" Mult]]
 
 binary :: String -> (a -> a -> a) -> Operator Parser a
@@ -220,16 +239,17 @@ spaceConsumer = L.space spaces empty empty
 spaces :: Parser ()
 spaces = some spc >> return ()
  where spc = string " " <|> string "\t" <|> string "\n" <|> string "\r"
-             <|> string "\\ " <|> string "~"
+             <|> string "\\ " <|> string "~"-} 
+
 
 -- generate problems 
 generateRandEx :: Int -> ChoiceTree FracExpr
 generateRandEx = undefined
 
-e1, e2, e3, e4, e5, e6 :: FracExpr
-e1 = FConst 0
-e2 = AndEvent (FVar 'A') (FVar 'B')
-e3 = OrEvent (FVar 'A') (FVar 'B')
-e4 = NegaEvent (FVar 'A') (FVar 'B')
-e5 = FVar 'C'
-e6 = OrEvent (AndEvent (FVar 'A') (FVar 'B')) ((FVar 'B'))
+example1, example2, example3, example4, example5, example6 :: FracExpr
+example1 = FConst 0
+example2 = AndEvent (FVar 'A') (FVar 'B')
+example3 = OrEvent (FVar 'A') (FVar 'B')
+example4 = NegaEvent (FVar 'A')
+example5 = FVar 'C'
+example6 = NegaEvent (AndEvent (FVar 'A') (FVar 'B'))
