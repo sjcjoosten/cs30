790e43d8fba4876f49648c8146dbf2dc25ae0502
Paul <paul.gralla@outlook.com>
2020-11-12 15:27:24 -0500

    Fix faulty exercise generation

--- src/CS30/Exercises/SetCardinalitiesProofs/ExerciseGeneration.hs
+++ src/CS30/Exercises/SetCardinalitiesProofs/ExerciseGeneration.hs
@@ -6,23 +6,26 @@ import Data.Char
 import CS30.Exercises.SetCardinalitiesProofs.Proof
 import CS30.Exercises.SetCardinalitiesProofs.RuleParser
 import qualified Data.Map as Map
-
+import GHC.Stack
+import Debug.Trace
+import Data.List.Extra
 
 
 setExercises :: [ChoiceTree ([Field], Integer)] -- first thing in field will be expression, rest its equivalencies
 setExercises = [fullExercise 3, fullExercise 5, fullExercise 7]
   where fullExercise i = do ex <- generateRandSetExpr i
-                            let Proof lhs steps = genProof laws ex
-                                (_,rhs) = last steps
+                            let Proof lhs steps = genProof laws (Op Cardinality [ex])
+                                rhs = last (lhs:map snd steps)
                             asgn <- genAllPossibleValues rhs 
                             let answer = evaluate asgn rhs
                             let exprs = (getExprs rhs) -- nubSort (getExprs rhs)
+                            trace ("asgn:" ++ show asgn ++ "\nlhs: " ++ show lhs ++ "\nexprs: " ++ show exprs) (return ())
                             return (genFields lhs (evaluate asgn) exprs, answer)
         
         genFields lhs getVal exprs
           = [FText "Given that "] 
             ++ combine [FMath (exprToLatex e ++ "=" ++ show (getVal e)) | e <- exprs]
-            ++ [FText ". Compute ", FMath (exprToLatex lhs)] 
+            ++ [FText ". Compute ", FMath (exprToLatex lhs)]
 
 
 -- taken from Raechel and Mahas Project
@@ -33,28 +36,40 @@ combine [x,y] = [x, FText " and ", y]
 combine [x,y,z] = [x, FText ", ", y, FText ", and ", z]
 combine (x:xs) = [x, FText ", "] ++ combine xs
 
-generateRandSetExpr :: Int -> ChoiceTree Expr
-generateRandSetExpr n 
-    | n < 2 = Branch [ Node (Var varName) | varName <- ['A' .. 'Z']]
+
+-- dont generate intersections
+-- if union,  only union
+-- intersection in cardinality will necessitate lookup
+
+generateRandSetExpr :: [Symb] -> Int -> ChoiceTree Expr
+generateRandSetExpr lstOfOps n 
+    | n < 2 = Branch [ Node (Var varName) | varName <- ['A' .. 'F']]
 generateRandSetExpr n = do {
-                            symb <- nodes [Intersection, Union, Powerset, Cartesian, Setminus];
-                            if symb == Powerset then
+                            symb <- nodes lstOfOps; -- [Union, Powerset, Cartesian, Setminus];
+                            if symb == Union then
                                 do {
-                                    expr <- generateRandSetExpr (n-1);
+                                    n' <- nodes [1 .. n-1];
+                                    expr1 <- generateRandSetExpr [Union] n';
+                                    expr2 <- generateRandSetExpr [Union] (n - n' - 2);
+                                    return (Op symb [expr1, expr2])
+                                }
+                            else if symb == Powerset then
+                                do {
+                                    expr <- generateRandSetExpr lstOfOps (n-1);
                                     return (Op symb [expr])
                                 }
                             else 
                                 do {
                                     n' <- nodes [1 .. n-1];
-                                    expr1 <- generateRandSetExpr n';
-                                    expr2 <- generateRandSetExpr (n - n' - 2);
+                                    expr1 <- generateRandSetExpr lstOfOps n';
+                                    expr2 <- generateRandSetExpr lstOfOps (n - n' - 2);
                                     return (Op symb [expr1, expr2])
                                 }
 } 
 
 generateQuestion :: ([Field], Integer) -> Exercise -> Exercise
 generateQuestion (myProblem,sol) def 
-    = def { eQuestion =  [FText "helloWorld"]--myProblem --, (FText [(show sol)]) 
+    = def { eQuestion =  myProblem--myProblem --, (FText [(show sol)]) 
           , eBroughtBy = ["Paul Gralla","Joseph Hajjar","Roberto Brito"] }
 
 
@@ -63,7 +78,7 @@ generateFeedback _ _ rsp = rsp
 
 cardinalityProofExer :: ExerciseType
 cardinalityProofExer = exerciseType "Set Cardinality" "L?.?" "Sets: Cardinalities"
-                            setEx
+                            setExercises
                             generateQuestion
                             generateFeedback
 
@@ -72,7 +87,7 @@ type PossibleVals = [(Expr, Integer)] -- Stores (expression, possible values) fo
 genAllPossibleValues :: Expr -> ChoiceTree PossibleVals
 genAllPossibleValues expr = assignAll toAssign
     where
-        toAssign = getExprs expr
+        toAssign = nubSort (getExprs expr)
         assignAll [] = Node []
         assignAll (x:xs) = do {
             possibleVal <- nodes [2 .. 20];
@@ -85,18 +100,20 @@ genAllPossibleValues expr = assignAll toAssign
 -- then it will compute
 
 --minus/plus/mult
-evaluate :: PossibleVals -> Expr -> Integer
+evaluate :: HasCallStack => PossibleVals -> Expr -> Integer
 evaluate _ (Var _) = error "Cannot evaluate var"
 evaluate _ (Val v) = v
-evaluate pV (Op Cardinality [expr]) = case (lookup expr pV) of
+evaluate pV expr@(Op Cardinality [_])
+                                = case (lookup expr pV) of
                                     Just i -> i
                                     Nothing -> error ("Content of cardinality needs to be in possible Values. expr in |expr| = " ++ (show expr))
 evaluate pV (Op Mult [e1,e2]) = (evaluate pV e1) * (evaluate pV e2)
 evaluate pV (Op Add [e1,e2]) = (evaluate pV e1) + (evaluate pV e2)
 evaluate pV (Op Sub [e1,e2]) = (evaluate pV e1) - (evaluate pV e2)
+evaluate _ expr = error ("Cannot evaluate expression: " ++ exprToLatex expr)
                             
 
 getExprs :: Expr -> [Expr]
-getExprs (Op Cardinality [e]) = [e]
+getExprs e@(Op Cardinality [_]) = [e]
 getExprs (Op _ exprs) = concatMap getExprs exprs
 getExprs (Var _) = error "Question is poorly phrased, a set is not a valid variable" -- If we have a set on its own in the expression, we throw an error
