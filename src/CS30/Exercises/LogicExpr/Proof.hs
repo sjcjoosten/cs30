e1b67a9a19d9f1289b7c04ff3604ebff8183afc2
sjcjoosten <sjcjoosten+github@gmail.com>
2020-11-16 12:56:18 -0500

    Squashed commit of the following:
    commit 2afd3c46da6d14966e79b69b0ef44ebecefc636b
    Author: Fei <i.james.guo@gmail.com>
    Date:   Mon Nov 16 11:03:08 2020 +0800
    
        some linting
    
    commit c66c796933ea49b2dd136c5069eb84df588a453d
    Merge: 7a53f2b 8e645da
    Author: Fei <i.james.guo@gmail.com>
    Date:   Fri Nov 13 11:38:55 2020 +0800
    
        Merge branch 'main' of https://github.com/jamesguo71/cs30 into main
    
    commit 7a53f2b324c677a5de103fea9270ddf6f918bc17
    Author: Fei <i.james.guo@gmail.com>
    Date:   Fri Nov 13 11:38:05 2020 +0800
    
        Uncomment a few "additional" laws
    
    commit 8e645da4af50d7a764b33185d1f40d04da90af3f
    Author: tvergho <tvergho@gmail.com>
    Date:   Thu Nov 12 19:37:56 2020 -0800
    
        Question wording change
    
    commit f4987ecc1183725acbe3602f01af87430aefd818
    Author: tvergho <tvergho@gmail.com>
    Date:   Thu Nov 12 14:35:30 2020 -0800
    
        Better feedback on first version of exercise, fixed URL bug
    
    commit 6790b178bdab207d2ca92bdcd2dc48c7c90046ee
    Author: tvergho <tvergho@gmail.com>
    Date:   Thu Nov 12 14:24:30 2020 -0800
    
        v2 of the exercise
    
    commit a291d43712ca4aa276e231e96812931b4dedc8e9
    Author: tvergho <tvergho@gmail.com>
    Date:   Thu Nov 12 12:57:35 2020 -0800
    
        Working exercise display without the filtering workaround
    
    commit 9e7d0168bad78a6e8de2582b4702e3a02cf5f69b
    Merge: e707261 37789f5
    Author: initialg <i.james.guo@gmail.com>
    Date:   Thu Nov 12 19:54:55 2020 +0800
    
        Merge pull request #3 from jamesguo71/fei_dev
    
        Update some methods in proof and parser
    
    commit 37789f56c044eab53de80d62cae44c61b5b490b7
    Author: Fei <i.james.guo@gmail.com>
    Date:   Thu Nov 12 19:53:25 2020 +0800
    
        update checkLaws implementation and added test for laws in test
    
    commit 249bc89613aebe51c236011c0423c11b3e6f3214
    Author: sjcjoosten <sjcjoosten+github@gmail.com>
    Date:   Wed Nov 11 21:46:47 2020 -0500
    
        Added test for all laws (fake and input)
    
    commit 71238872916d6767b808ab60eb27579ab4f440ac
    Author: Fei <i.james.guo@gmail.com>
    Date:   Thu Nov 12 19:28:05 2020 +0800
    
        update genFaultyProof; tweak display of expr;
    
    commit e7072617cabc779e3bb0558de9fd718a9cf3519d
    Author: tvergho <tvergho@gmail.com>
    Date:   Wed Nov 11 20:46:52 2020 -0800
    
        Randomly ordered proof and displaying feedback
    
    commit 31cebdbe26f0a318e89ddf5d06d9a9e8499941b8
    Author: Fei <i.james.guo@gmail.com>
    Date:   Wed Nov 11 22:19:01 2020 +0800
    
        Implemented evaluate func; try to generate faulty proof
    
    commit d4d3970c964223c4a1bcd5271ff32145cb194323
    Author: tvergho <tvergho@gmail.com>
    Date:   Mon Nov 9 19:08:16 2020 -0800
    
        Fixed warnings
    
    commit dd0178ec04b9beaa94d9c75cb06fd443365b645e
    Author: tvergho <tvergho@gmail.com>
    Date:   Fri Nov 6 18:09:27 2020 -0800
    
        Basic expression generation and connection to web interface
    
    commit cd0636ede2acf117d4f91de5a1c939f842186497
    Merge: b1003d4 5b21ece
    Author: initialg <i.james.guo@gmail.com>
    Date:   Thu Nov 5 18:13:58 2020 +0800
    
        Merge pull request #2 from sjcjoosten/main
    
        Proof display (#31)
    
    commit b1003d46c1ac0711b2785169119fe47b934c285f
    Merge: 3828421 a53117c
    Author: initialg <i.james.guo@gmail.com>
    Date:   Thu Nov 5 00:13:12 2020 +0800
    
        Merge pull request #1 from jamesguo71/fei_dev
    
        Refactored proof code
    
    commit a53117c370617fb44e324906ce48a14ce08289d3
    Author: Fei <i.james.guo@gmail.com>
    Date:   Thu Nov 5 00:10:09 2020 +0800
    
        Rename Logic to Proof; Added a few assoc laws
    
    commit d3f13dabd660e9c7701d34d7b1b32084950afac9
    Author: Fei <i.james.guo@gmail.com>
    Date:   Wed Nov 4 21:06:27 2020 +0800
    
        add some test examples
    
    commit 2af6ee482ad94b22b70ac36915439bf4e46225f0
    Author: Fei <i.james.guo@gmail.com>
    Date:   Wed Nov 4 21:04:44 2020 +0800
    
        A lot of Refactoring
    
    commit 3828421ef92dc4adcdb5493c5cacd5c5932e33ef
    Author: Fei <i.james.guo@gmail.com>
    Date:   Mon Nov 2 13:28:10 2020 +0800
    
        alter the precedence of implication
    
    commit ca256b110a1564622b8e791ffc65c84153e59b9a
    Author: Fei <i.james.guo@gmail.com>
    Date:   Mon Nov 2 11:39:51 2020 +0800
    
        Parse logic and try to generate a proof (buggy now)

--- src/CS30/Exercises/LogicExpr/Proof.hs
+++ src/CS30/Exercises/LogicExpr/Proof.hs
@@ -3,49 +3,103 @@
 module CS30.Exercises.LogicExpr.Proof where
 import CS30.Exercises.LogicExpr.Parser hiding (law)
 import Data.Maybe ( fromJust )
-import CS30.Exercises.Data
 
 type Substitution = [(VarName,LogicExpr)]
 type VarName = Char
 
-data Proof = Proof LogicExpr [Step] deriving (Show)
+data Proof a = Proof LogicExpr [Step a] deriving (Show)
 
-type Step = (LawName, LogicExpr)
+type Step a= (a, LogicExpr)
 
 input_laws :: [String]
 input_laws = [
-    "Law1:¬(¬p)≡p"
-    , "Law2:p∧p≡p"
-    , "Law3:p∧true≡p"
-    , "Law4:p∨true≡true"
-    , "Law5:p∨p≡p"
-    , "Law6:(p ⇒ q) ≡ ¬p ∨ q"
-    , "Law7:p∨false≡p"
-    , "Law8:p∧false≡false"
-    , "Law9:¬(p∨q)≡¬p∧¬q"
-    , "Law10:¬(p∧q)≡¬p∨¬q"
-    , "Law11:p∧¬p≡false"
-    , "Law12:p∨¬p≡true"
+    "Negation of Negation:¬(¬p)≡p"
+    , "Idempotence:p∧p≡p"
+    , "Operation with true,false:p∧true≡p"
+    , "Operation with true,false:p∨true≡true"
+    , "Idempotence:p∨p≡p"
+    , "Implications as an OR:(p ⇒ q) ≡ ¬p ∨ q"    
+    -- Easy challenge: only apply law when a negation before it; 
+    -- Not sure if this makes proof more interesting, so commented out for now
+    -- , "Implications as an OR:¬(p⇒q)≡¬(¬p∨q)"    
+    , "Operation with true,false:p∨false≡p"
+    , "Operation with true,false:p∧false≡false"
+    , "DeMorgan's Law:¬(p∨q)≡¬p∧¬q"
+    , "DeMorgan's Law:¬(p∧q)≡¬p∨¬q"
+    , "Operation with Negation:p∧¬p≡false"
+    , "Operation with Negation:p∨¬p≡true"
+
+    -- Below are not in the given law list 
     , "Redundancy law: (p∨q)∧(p∨¬q)≡p"
     , "Redundancy law: p∨(¬p∧q)≡p∨q"
-    , "true/false: ¬true≡false"
-    , "true/false: ¬false≡true"
-    , "Assoc: (p∨q)∨r≡p∨q∨r"
-    , "Assoc: p∨(q∨r)≡p∨q∨r"    
-    , "Assoc: (p∧q)∧r≡p∧q∧r"
-    , "Assoc: p∧(q∧r)≡p∧q∧r"
+    , "Complement Law: ¬true≡false"
+    , "Complement Law: ¬false≡true"
+    
+    -- Optional Distributivity    
+    -- , "Distributivity:p∨(q∧r)≡(p∨q)∧(p∨r)"
+    -- , "Distributivity:(q∧r)∨p≡(q∨p)∧(r∨p)"
+
+    -- , "Associativity: (p∨q)∨r≡p∨(q∨r)"
+    -- , "Associativity: p∨(q∨r)≡(p∨q)∨r"    
+    -- , "Associativity: (p∧q)∧r≡p∧(q∧r)"
+    -- , "Associativity: p∧(q∧r)≡(p∧q)∧r"
     -- Commutative laws give potentially verbose proof
-    -- , "Commute: p∧q≡q∧p"
-    -- , "Commute: p∨q≡q∨p"
+    -- , "Commutativity: p∧q≡q∧p"
+    -- , "Commutativity: p∨q≡q∨p"    
     ]
 
-simplify :: [String] -> String -> Proof
+fake_laws :: [String]
+fake_laws = [
+    "FAKELAW: ¬(p ⇒ q) ≡ ¬p ⇒ ¬q"
+    , "FAKELAW: p∨(q∧r)≡(p∨q)∧r"
+    , "FAKELAW: (p∧q)∨r≡p∧(q∨r)"
+    , "FAKELAW: ¬(p∧q)≡(p∨q)"
+    , "FAKELAW: ¬(p∧q)≡(¬p∧¬q)"
+    , "FAKELAW: ¬(p⇒q)≡(¬p⇒¬q)"
+    , "FAKELAW: ¬(p⇒q)≡(q⇒p)"
+    , "FAKELAW: ¬(p⇒q)≡(¬q⇒¬p)"
+    , "FAKELAW: false⇒q≡¬q"
+  ]
+
+renameLaw :: (a -> b) -> Law a -> Law b
+renameLaw f law = law {lawName=f (lawName law)}
+
+-- try to generate a faulty proof for an expr
+genFaultyProof :: LogicExpr -> Proof Bool
+genFaultyProof = getWrongDerivation true_laws false_laws
+  where 
+    true_laws = map (renameLaw (const True) . parseLaw) input_laws
+    false_laws = map (renameLaw (const False) . parseLaw) fake_laws
+
+-- Check if a list of laws are all true
+checkLaws :: [String] -> Bool
+checkLaws = all checkLaw
+checkLaw :: String -> Bool
+checkLaw law = case parseLaw law of
+                 (Law _ (lhs, rhs)) -> equivalenceCheck lhs rhs
+
+-- Simlify an expr with given laws
+simplify :: [String] -> String -> Proof LawName
 simplify strings str = getDerivation laws e
   where
     laws = map parseLaw strings
     e = parseExpr str
 
-getDerivation :: [Law] -> LogicExpr -> Proof
+getWrongDerivation :: [Law Bool] -> [Law Bool] -> LogicExpr -> Proof Bool
+getWrongDerivation true_laws false_laws e
+ = Proof e (multiSteps e 0)
+ where multiSteps :: LogicExpr -> Int -> [Step Bool] 
+       multiSteps e' stepNum
+        = case [ (lawName law, res)
+               | law <- if even stepNum then true_laws++false_laws else false_laws++true_laws -- true_laws and false_laws in alternatining order of appearance
+               , res <- getStep (lawEq law) e'               
+               , lawName law || not (equivalenceCheck e' res)
+               ] of
+            [] -> []
+            (x:_xs) -> x:multiSteps (snd x) (stepNum+1)
+
+
+getDerivation :: [Law a] -> LogicExpr -> Proof a
 getDerivation laws e
  = Proof e (multiSteps e []) -- Give multiSteps an accumulater param, for the use of commutative law;
  where multiSteps e' acc
@@ -96,15 +150,15 @@ matchE (Bin op1 e1 e2) (Bin op2 e3 e4) | op1 == op2
           s2 <- matchE e2' e4'
           combineTwoSubsts s1 s2 
 
-matchE (Bin _ _ _) _ = Nothing
+matchE Bin {} _ = Nothing
 matchE (Neg e1) (Neg e2) = matchE e1 e2
 matchE (Neg _) _ = Nothing
 
 combineTwoSubsts :: Substitution -> Substitution -> Maybe Substitution
 combineTwoSubsts s1 s2
-  = case and [v1 == v2 | (nm1,v1) <- s1, (nm2,v2) <- s2, nm1 == nm2] of
-     True -> Just (s1 ++ s2)
-     False -> Nothing
+  = if and [v1 == v2 | (nm1,v1) <- s1, (nm2,v2) <- s2, nm1 == nm2]
+    then Just (s1 ++ s2)
+    else Nothing
 
 apply :: Substitution -> LogicExpr -> LogicExpr
 apply subst (Var nm) = fromJust $ lookup nm subst
