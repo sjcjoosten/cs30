e7a00e2b1509efe19e5f4e1cfb19469243afa4a7
sjcjoosten <sjcjoosten+github@gmail.com>
2020-11-16 11:29:12 -0500

    Added trace import

--- test/test.hs
+++ test/test.hs
@@ -8,6 +8,7 @@ import           Test.Tasty
 -- import Test.Tasty.SmallCheck as SC
 import           Test.Tasty.QuickCheck as QC
 import           Text.TeXMath.Readers.TeX (readTeX)
+import Debug.Trace
 
 main :: IO ()
 main = defaultMain tests
